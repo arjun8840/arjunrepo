6b414143ce297418489d338df5d184f3d1fc3e89
arjun8840 <er_arjun2006@yahoo.com>
2019-05-27 22:18:03 +0530

    create variables.tf

---
+++ modules/services/ec2/variables.tf
@@ -0,0 +1,9 @@
+variable "server_port" {
+  description = "Open all the ports"
+  type        = "string"
+  default     = "22"
+}
+
+variable "ec2_instance_name" {
+description = "Pls enter the purpose of EC2 instance"
+}
